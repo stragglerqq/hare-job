4f887c069e0987310e9d1d3834ed5f72aef7b7ab
Sebastian <sebastian_zajac@hotmail.com>
2020-02-13 00:48:00 +0100

    feature: Job state extended

--- src/app/core/job/interface/jobDto.ts
+++ src/app/core/job/interface/jobDto.ts
@@ -1,4 +1,25 @@
 export interface JobDto {
   readonly id: string;
   readonly name: string;
+  readonly title: string;
+  readonly posted: string;
+  readonly renewed: string;
+  readonly logo: string;
+  readonly seniority: string;
+  readonly url: string;
+  readonly salary: Salary;
+  readonly technologyIds: number[];
+  readonly categoryIds: number[];
+}
+
+export interface Salary {
+  from: number;
+  to: number;
+  currency: Currency;
+}
+
+export enum Currency {
+   PLN,
+   USD,
+   EUR
 }
--- src/app/core/job/state/job.model.ts
+++ src/app/core/job/state/job.model.ts
@@ -4,11 +4,15 @@ import { JobDto } from '@core/job/interface';
 export interface Job {
   id: ID;
   name: string;
+  categoryIds: ID[];
+  technologyIds: ID[];
 }
 
-export function createJob({ id, name }: JobDto): Job {
+export function createJob({ id, name, categoryIds, technologyIds }: JobDto): Job {
   return {
     id,
     name,
+    categoryIds,
+    technologyIds,
   };
 }
