cde574a747f7b03a198dbbe2657168834c3c06cc
Sebastian Zajac <s.zajac@nowaera.pl>
2020-02-13 23:31:57 +0100

    feature: State management with jobs

--- src/app/jobs/jobs-board/jobs-board.component.ts
+++ src/app/jobs/jobs-board/jobs-board.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { JobQuery } from '@core/job/state';
+import { Category, CategoryQuery } from '@core/category/state';
+import { Technology, TechnologyQuery } from '@core/technology/state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-jobs-board',
   templateUrl: './jobs-board.component.html',
   styleUrls: ['./jobs-board.component.scss']
 })
-export class JobsBoardComponent implements OnInit {
+export class JobsBoardComponent {
+  job$ = this.jobQuery.selectJob$;
 
-  constructor() { }
+  constructor(
+    private readonly jobQuery: JobQuery,
+    private readonly categoryQuery: CategoryQuery,
+    private readonly technologyQuery: TechnologyQuery
+  ) { }
 
-  ngOnInit(): void {
+  getCategory(id: Category['id']): Observable<Category> {
+    return this.categoryQuery.selectEntity(id);
   }
 
+  getTechnology(id: Technology['id']): Observable<Technology> {
+    return this.technologyQuery.selectEntity(id);
+  }
 }
