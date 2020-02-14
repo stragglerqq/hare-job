ef9844c981fd3e50ed3557373ceed135c90eca7c
Sebastian Zajac <s.zajac@nowaera.pl>
2020-02-14 09:29:41 +0100

    feature: State management with jobs

--- src/app/jobs/jobs-board/jobs-board.component.ts
+++ src/app/jobs/jobs-board/jobs-board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { JobQuery } from '@core/job/state';
 import { Category, CategoryQuery } from '@core/category/state';
 import { Technology, TechnologyQuery } from '@core/technology/state';
